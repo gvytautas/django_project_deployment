c910b26de8773c35575673d5ed7a8db524c42d98
gvytautas <gvytautas90@gmail.com>
2023-11-08 20:49:41 +0200

    debug enabled static root set

--- django_project_deployment/settings.py
+++ django_project_deployment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+sv_=@65f1a&qe76lb$zd)9qt(%igm(-260noqygba591i=aq1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['165.227.131.242']
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
